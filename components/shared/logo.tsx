d1f6d1d626f94e8fca14d3bfa937d37b820027fb
ademsuslu <ademsuslu9080@gmail.com>
2024-12-17 22:03:23 +0300

    refactor:ogo for useEffect add cause shoul be changed

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -3,12 +3,17 @@
 import { useTheme } from "next-themes"
 import Link from 'next/link'
 import Image from 'next/image'
+import { useEffect } from "react"
 const Logo = () => {
   const { theme } = useTheme()
-  let themes = theme || "dark"
+  useEffect(() => {
+  
+  }, [theme])
+  
+
   return (
     <div>
-      {themes === "dark" ?
+      {theme === "dark" ?
        <Link  href={"/"} >
            <Image width={158} className="!min-w-[158px]" height={36} src="/image.png" alt="Dark_logo" />
         </Link>
