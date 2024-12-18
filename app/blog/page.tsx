9b1b428052ded9379f1c1fcfa9446d0f1ac87f01
ademsuslu <ademsuslu9080@gmail.com>
2024-12-19 01:48:28 +0300

    feat:hero

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,14 @@
 import BreadCrumps from '@/components/shared/breadCrumps'
+import Hero from '@/components/shared/Hero'
 import React from 'react'
 
 export default function BlogPage() {
   return (
     <div>
       <BreadCrumps/>
-      Hero
+      <div className="mt-12"/>
+      <Hero/>
+      <div className="mt-12"/>
       LastPost List
       reklam
     </div>
---
+++ components/shared/Hero.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import React from 'react'
+import { Button } from '../ui/button'
+
+const Hero = () => {
+    return (
+        <div className='hero min-h-[450px] flex  items-end'>
+            <div className='flex  flex-col max-w-[720px] m-10'>
+                <div className="mb-4">
+                    <Button  className="text-white">Technology</Button>
+                </div>
+                <h1 className="text-[36px] font-bold hidden md:block">
+                    The Impact of Technology on the Workplace: How Technology is Changing
+                </h1>
+                <div className="flex items-center mt-6">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>TW</AvatarFallback>
+                    </Avatar>
+                    <span className="ml-3">Tracey Wilson</span>
+                    <span className="ml-4">August 20, 2022</span>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
