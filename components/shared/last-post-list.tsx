091c86b427ab1382b9b8e82bc4e23d1ee46b198c
ademsuslu <ademsuslu9080@gmail.com>
2024-12-19 01:53:06 +0300

    refactor:shadcn in add card comp

---
+++ components/shared/last-post-list.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LastPostList = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+        <div className="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, harum! Adipisci obcaecati fuga vel dolore consectetur quidem vero quia eius! Voluptate modi ipsa velit magni, deserunt quam delectus minima in.</div>
+    </div>
+  )
+}
+
+export default LastPostList
