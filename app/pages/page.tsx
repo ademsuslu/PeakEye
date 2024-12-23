9eb9e8bc04bab6ccab7c8865111adfd41d90f837
ademsuslu <ademsuslu9080@gmail.com>
2024-12-24 00:37:56 +0300

    refactor:pages page ın getnewsletter log deleted

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { handleGetNewsletter } from '../actions/getAll'
 
 export default async function PagesPage() {
- const res= await handleGetNewsletter()
 
- console.log(res)
   return (
     <div>PagesPage</div>
   )
